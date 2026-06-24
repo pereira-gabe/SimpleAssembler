8577790dba19e34935c3813ce3bd68674683ca72
Hadrian Gabriel Pereira <hadriangabriel.trsp@gmail.com>
2026-06-23 23:04:46 -0300

    .

--- Processor/RiscVProcessor.c
+++ Processor/RiscVProcessor.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define MEM_SIZE 1024
+#define MAX_INSTRUCTIONS 1024
 
-uint32_t memory[MEM_SIZE];
+uint32_t program[MAX_INSTRUCTIONS];
 uint32_t reg[32];
 uint32_t pc = 0;
 
-static const char* abi_convention[32] = {
+static const char* reg_names[32] = {
     "zero", "ra", "sp", "gp", "tp",
     "t0", "t1", "t2",
     "s0", "s1",
@@ -25,13 +25,13 @@ int32_t sign_extend(int32_t value, int bits) {
 void print_registers() {
     printf("============ Registers Table ============\n");
 
-    for(int i = 0; i < 16; i++) {
+    for (int i = 0; i < 16; i++) {
         printf(
-            "%-4s (x%-2d) = %-2d     |     %-4s (x%-2d) = %d\n",
-            abi_convention[i],
+            "%-4s (x%-2d) = %-10d | %-4s (x%-2d) = %d\n",
+            reg_names[i],
             i,
             reg[i],
-            abi_convention[i + 16],
+            reg_names[i + 16],
             i + 16,
             reg[i + 16]
         );
@@ -49,66 +49,53 @@ void execute(uint32_t instruction) {
     uint32_t funct7 = (instruction >> 25) & 0x7F;
 
     int32_t imm_i = sign_extend((instruction >> 20) & 0xFFF, 12);
-    int32_t imm_s = sign_extend((((instruction >> 25) & 0x7F) << 5) |((instruction >> 7) & 0x1F),12);
-    int32_t imm_b = sign_extend((((instruction >> 31) & 0x1) << 12) |(((instruction >> 7) & 0x1) << 11) |(((instruction >> 25) & 0x3F) << 5) |(((instruction >> 8) & 0xF) << 1),13);
 
-    switch(opcode) {
-        case 0x33:     // R-Type
-            switch(funct3) {
+    int32_t imm_b = sign_extend(
+        (((instruction >> 31) & 0x1) << 12) |
+        (((instruction >> 7)  & 0x1) << 11) |
+        (((instruction >> 25) & 0x3F) << 5) |
+        (((instruction >> 8)  & 0xF) << 1),
+        13
+    );
+
+    switch (opcode) {
+
+        case 0x33:
+            switch (funct3) {
+
                 case 0x0:
-                    if(funct7 == 0x00) {        // ADD
+                    if (funct7 == 0x00)
                         reg[rd] = reg[rs1] + reg[rs2];
-                    }
-
-                    else if(funct7 == 0x20) {   // SUB
+                    else if (funct7 == 0x20)
                         reg[rd] = reg[rs1] - reg[rs2];
-                    }
-
                     break;
-                case 0x7:                       // AND
+
+                case 0x7:
                     reg[rd] = reg[rs1] & reg[rs2];
                     break;
-                case 0x6:                       // OR
+
+                case 0x6:
                     reg[rd] = reg[rs1] | reg[rs2];
                     break;
             }
+
             pc += 4;
             break;
 
-        case 0x13:     // I-Type
-            if(funct3 == 0x0) {                 // ADDI
-                reg[rd] = reg[rs1] + imm_i;
-            }
+        case 0x13:
+            reg[rd] = reg[rs1] + imm_i;
             pc += 4;
             break;
 
-        case 0x03:     // LOAD
-            if(funct3 == 0x2) {                 // LW
-                reg[rd] = memory[(reg[rs1] + imm_i) / 4];
-            }
-            pc += 4;
-            break;
-
-        case 0x23:     // STORE
-            if(funct3 == 0x2) {                 // SW
-                memory[(reg[rs1] + imm_s) / 4] = reg[rs2];
-            }
-            pc += 4;
-            break;
-
-        case 0x63:     // BRANCH
-            if(funct3 == 0x0) {                 // BEQ
-                if(reg[rs1] == reg[rs2]) {
-                    pc += imm_b;
-                }
-                else {
-                    pc += 4;
-                }
-            }
+        case 0x63:
+            if (reg[rs1] == reg[rs2])
+                pc += imm_b;
+            else
+                pc += 4;
             break;
 
         default:
-            printf("unsuported instruction\n");
+            printf("unsupported instruction: 0x%08X\n", instruction);
             exit(1);
     }
 
@@ -123,20 +110,19 @@ int main() {
         return 1;
     }
 
-    int instructions = 0;
+    int instruction_count = 0;
 
-    while (instructions < MEM_SIZE && fscanf(file, "%x", &memory[instructions]) == 1) instructions++;
+    while (
+        instruction_count < MAX_INSTRUCTIONS &&
+        fscanf(file, "%x", &program[instruction_count]) == 1
+    ) {
+        instruction_count++;
+    }
 
     fclose(file);
 
-    if (instructions > MEM_SIZE / 8){
-        printf("Instructions limit exceeded.\nMax number of instructions supported: %d", MEM_SIZE);
-        return 2;
-    }
-
-    while(pc < instructions * 4) {
-        uint32_t instruction = memory[pc / 4];
-        execute(instruction);
+    while (pc < instruction_count * 4) {
+        execute(program[pc / 4]);
     }
 
     print_registers();
