66d84d8a67386dcf795c6e980f947cfecfcff567
gabs <hadriangabriel.trsp@gmail.com>
2026-06-23 22:58:38 -0300

    Refactor assembler into structured two-pass design

--- Assembler/Assembler.c
+++ Assembler/Assembler.c
@@ -3,26 +3,37 @@
 #include <stdint.h>
 #include <string.h>
 
-#define MAX_CHARACTERS 60
-#define DEBUG 0
-#define LABEL_FOUND 0xFFFFFFFF
-#define INVALID_INSTRUCTION 0xFFFFFFFE 
+#define MAX_LINE 128
+#define N_INSTRUCTIONS 6 // instruction set size
 
-static int first = 1;
-static uint32_t beg_offset = 0; // current offset from the first instruction of the segment
-
-typedef struct Label{
+typedef struct{
     char* name;
     int32_t offset;
 } Label;
 
-static uint32_t label_list_size = 0;
-Label* label_list = NULL;
+typedef struct{
+    FILE* instructions;
+    FILE* machine_code;
+    Label* labels;
+    int nlabels;
+    uint32_t offset;
+} Assembler;
 
-FILE *instructions = NULL; 
-FILE *code = NULL; 
+typedef struct{
+    char mnemonic[16];
 
-static const char* abi_convention[32] = {
+    uint32_t opcode;
+    uint32_t funct3;
+    uint32_t funct7;
+
+    uint32_t rd;
+    uint32_t rs1;
+    uint32_t rs2;
+
+    int32_t imm;
+} Instruction;
+
+static const char* registers[32] = {    // TODO: hash table implementation of reg table
     "zero", "ra", "sp", "gp", "tp",
     "t0", "t1", "t2",
     "s0", "s1",
@@ -31,65 +42,38 @@ static const char* abi_convention[32] = {
     "t3", "t4", "t5", "t6"
 };
 
-void initialize(){
-    instructions = fopen("instructions.txt", "r");
-    code = fopen("machine_code.txt", "w");
+Instruction instruction_set[] = {
+    {.mnemonic="sub",  .opcode=0x33, .funct3=0x0, .funct7=0x20},
+    {.mnemonic="add",  .opcode=0x33, .funct3=0x0, .funct7=0x00},
+    {.mnemonic="and",  .opcode=0x33, .funct3=0x7, .funct7=0x00},
+    {.mnemonic="or",   .opcode=0x33, .funct3=0x6, .funct7=0x00},
+    {.mnemonic="addi", .opcode=0x13, .funct3=0x0},
+    {.mnemonic="beq",  .opcode=0x63, .funct3=0x0}
+};
+
+void initialize(Assembler* as, const char* inst_file, const char* out_file){ // TODO: proper error handling
+    as->instructions = fopen(inst_file, "r");
+    if (as->instructions == NULL){
+        printf("failed to open instructions file\n");
+        exit(10);
+    }
+    as->machine_code = fopen(out_file, "w");
+    if (as->machine_code == NULL){
+        printf("failed to open instructions file\n");
+        exit(10);
+    }
+    as->labels = NULL;
+    as->nlabels = 0;
+    as->offset = 0;
 }
 
-void print_hex(uint32_t value){
-    if (first) {
-        printf("Instructions parsed:\n");
-        first--;
-    }
-    printf("0x%08X\n", value);
-}
-
-void print_labels(){
-    for (int i = 0; i < label_list_size; i++){
-        printf("%s, %d\n", label_list[i].name, label_list[i].offset);
-    }
-}
-
-int contains(const char* s, char target){
-    int i = 0;
-    while (s[i] != '\n' && s[i] != '\0'){
-        if (s[i] == target) return 1;
-        i++;
-    }
-    return 0;
-}
-
-uint32_t check_op(const char *op){
-    if (strcmp(op, "add") == 0 || strcmp(op, "sub") == 0 || strcmp(op, "and") == 0 || strcmp(op, "or") == 0) return 0x33;
-    if (strcmp(op, "addi") == 0) return 0x13;
-    if (strcmp(op, "beq") == 0) return 0x63;
-    
-    return 0x0;
-}
-
-void return_funct(const char *op, uint32_t* funct3, uint32_t* funct7){
-    if (strcmp(op, "add") == 0){
-        *funct3 = 0x0;
-        *funct7 = 0x0;
-    }
-    if (strcmp(op, "sub") == 0){
-        *funct3 = 0x0;
-        *funct7 = 0x20;
-    }
-    if (strcmp(op, "and") == 0){
-        *funct3 = 0x7;
-        *funct7 = 0x0;
-    }
-    if (strcmp(op, "or") == 0){
-        *funct3 = 0x6;
-        *funct7 = 0x0;
+void print_labels(Assembler* as){
+    for (int i = 0; i < as->nlabels; i++){
+        printf("%s, %d\n", as->labels[i].name, as->labels[i].offset);
     }
 }
 
-void return_funct3(const char *op, uint32_t* funct3){
-    if (strcmp(op, "addi") == 0 || strcmp(op, "beq") == 0) *funct3 = 0x0;
-}
-
+/* swapped for reg intepretation approach
 void replace_substring(char *s1, const char *old, const char *s2) {
     char *pos = strstr(s1, old);
 
@@ -100,246 +84,182 @@ void replace_substring(char *s1, const char *old, const char *s2) {
 
     memmove(pos + new_len, pos + old_len, strlen(pos + old_len) + 1);
     memcpy(pos, s2, new_len);
-}
+}*/
 
-void replace_registers(char *inst) {
-    char old[8];
-    char new_reg[8];
+void first_pass(Assembler* as){
+    uint32_t offset = 0;
+    char line[MAX_LINE];
 
-    for (int reg = 0; reg < 32; reg++) {
-        char *p = inst;
+    while (fgets(line, sizeof(line), as->instructions) != NULL){
+        char* comment = strchr(line, '#');
 
-        while ((p = strstr(p, abi_convention[reg])) != NULL) {
-            size_t len = strlen(abi_convention[reg]);
+        if (comment != NULL)
+            *comment = '\0';
 
-            char before = (p == inst) ? '\0' : p[-1];
-            char after  = p[len];
+        if (strspn(line, " \t\r\n") == strlen(line)) // skip blank lines
+            continue;
 
-            int left_ok =
-                before == '\0' ||
-                before == ' '  ||
-                before == ','  ||
-                before == '('  ||
-                before == ')'  ||
-                before == '\t' ||
-                before == '\n';
+        if (strchr(line, ':') != NULL){ // label: instruction is ignored
+            char* label = strtok(line, ":");
 
-            int right_ok =
-                after == '\0' ||
-                after == ' '  ||
-                after == ','  ||
-                after == '('  ||
-                after == ')'  ||
-                after == '\t' ||
-                after == '\n';
+            as->nlabels++;
+            as->labels = realloc(
+                as->labels,
+                as->nlabels * sizeof(*as->labels)
+            );
 
-            if (left_ok && right_ok) {
-                strcpy(old, abi_convention[reg]);
-                sprintf(new_reg, "x%d", reg);
-
-                replace_substring(inst, old, new_reg);
-
-                p = inst; 
-            } else p++;
+            as->labels[as->nlabels - 1].name = strdup(label);
+            as->labels[as->nlabels - 1].offset = offset;
+        }
+        else{
+            offset += 4;
         }
     }
+
+    rewind(as->instructions);
 }
 
-void first_pass(){  // the translation of the abi reg names is done on the second pass
-    int offset = 0;
-    char str[MAX_CHARACTERS];
-    while(fgets(str, MAX_CHARACTERS, instructions) != NULL){
-        if (contains(str, ':')){
-            char* colon = strchr(str, ':');
-            *colon = '\0';
-            label_list = realloc(label_list, (label_list_size + 1) * sizeof(*label_list));
-            label_list[label_list_size].name = malloc(MAX_CHARACTERS);
-            strcpy(label_list[label_list_size].name, str);
-            label_list[label_list_size].offset = offset;
-            label_list_size++;
+int32_t return_label_offset(Assembler* as, char* label, int offset){
+    for (int i = 0; i < as->nlabels; i++) {
+        if (strcmp(as->labels[i].name, label) == 0)
+            return as->labels[i].offset - offset;
+    }
+    printf("label on line %d not found\n", (as->offset / 4));
+    exit(3);
+}
+
+Instruction* find_instruction(const char *mnemonic){
+    for (int i = 0; i < N_INSTRUCTIONS; i++){
+        if (strcmp(mnemonic, instruction_set[i].mnemonic) == 0)
+            return &instruction_set[i];
+    }
+
+    return NULL;
+}
+
+int parse_register(const char *reg){
+    if (reg[0] == 'x'){
+        int n = atoi(reg + 1);
+
+        if (n >= 0 && n <= 31)
+            return n;
+    }
+
+    for (int i = 0; i < 32; i++){
+        if (strcmp(reg, registers[i]) == 0)
+            return i;
+    }
+
+    printf("Invalid register %s\n", reg);
+    exit(1);
+}
+
+void second_pass(Assembler* as){
+    char line[MAX_LINE];
+
+    while(fgets(line, sizeof(line), as->instructions) != NULL){
+        uint32_t result = 0x0;
+
+        if (strchr(line, ':') != NULL) // label: instruction is ignored
+            continue;
+
+        char* comment = strchr(line, '#');
+
+        if (comment != NULL)
+            *comment = '\0';
+
+        char *tokens[10];
+        int n_tokens = 0;
+
+        char *tok = strtok(line, " ,\t\n");
+
+        while (tok != NULL && n_tokens < 8){
+            tokens[n_tokens++] = tok;
+            tok = strtok(NULL, " ,\t\n");
         }
-        else offset += 4;   
+
+        if (n_tokens == 0)
+            continue;
+
+        Instruction *inst = find_instruction(tokens[0]);
+
+        if (inst == NULL){
+            printf("Unsupported instruction %s on line %u\n", tokens[0], (as->offset / 4) + 1);
+            exit(1);
+        }
+
+        if (inst->opcode == 0x33){ // TYPE R
+            int rd  = parse_register(tokens[1]);
+            int rs1 = parse_register(tokens[2]);
+            int rs2 = parse_register(tokens[3]);
+
+            result =
+                inst->opcode |
+                (rd << 7) |
+                (inst->funct3 << 12) |
+                (rs1 << 15) |
+                (rs2 << 20) |
+                (inst->funct7 << 25);
+        }
+
+        else if (inst->opcode == 0x13){ // TYPE I
+            int rd  = parse_register(tokens[1]);
+            int rs1 = parse_register(tokens[2]);
+            int imm = atoi(tokens[3]);
+
+            result =
+                inst->opcode |
+                (rd << 7) |
+                (inst->funct3 << 12) |
+                (rs1 << 15) |
+                ((imm & 0xFFF) << 20);
+        }
+
+        else if (inst->opcode == 0x63){ // TYPE B
+            int rs1 = parse_register(tokens[1]);
+            int rs2 = parse_register(tokens[2]);
+
+            int imm = return_label_offset(
+                as,
+                tokens[3],
+                as->offset
+            );
+
+            result =
+                inst->opcode |
+                (inst->funct3 << 12) |
+                (rs1 << 15) |
+                (rs2 << 20);
+
+            result |= ((imm >> 12) & 0x1) << 31;
+            result |= ((imm >> 5)  & 0x3F) << 25;
+            result |= ((imm >> 1)  & 0xF) << 8;
+            result |= ((imm >> 11) & 0x1) << 7;
+        }
+
+        as->offset += 4;
+        char inst_str[12];
+        sprintf(inst_str, "0x%08X\n", result);
+        //printf("%s", inst_str);
+        fputs(inst_str, as->machine_code);
     }
 }
 
-int return_label_offset(char* label, int offset){
-    for (int i = 0; i < label_list_size; i++) {
-        if (strcmp(label_list[i].name, label) == 0) return label_list[i].offset - offset;
-    }
-    printf("label on line %d not found\n", (beg_offset / 4));
-    exit(2);
-}
-
-uint32_t assemble(char *instruction){
-    uint32_t result = 0x0;
-    uint32_t op, rd, rs1, rs2, funct3, funct7;
-    int32_t imm_i, imm_s;
-    
-    if (contains(instruction, ':')){   // if the line represents a label
-        return LABEL_FOUND;
-    }
-
-    replace_registers(instruction);
-
-    char op_str[MAX_CHARACTERS], rd_str[10], rs1_str[10], rs2_str[10], imm_i_str[10], imm_s_str[20];
-    int i, j;
-
-    for (i = 0; instruction[i] != ' ' && instruction[i] != '\0'; i++) op_str[i] = instruction[i];
-    op_str[i] = '\0';
-
-    uint32_t current_pc = beg_offset;
-    beg_offset += 4;    // labels don't change the offset
-    op = check_op(op_str);
-
-    // #### TYPE R
-    if (op == 0x33){   
-        j = 0;
-        while(instruction[i] != 'x') i++;
-        i++;
-        for (; instruction[i] != ','; i++, j++) rd_str[j] = instruction[i];
-        rd_str[j] = '\0';
-        rd = atoi(rd_str);
-        while(instruction[i] != 'x') i++;
-        i++;
-        j = 0;
-
-        for (; instruction[i] != ','; i++, j++) rs1_str[j] = instruction[i];
-        rs1_str[j] = '\0';
-        rs1 = atoi(rs1_str);
-        while(instruction[i] != 'x') i++;
-        i++;
-        j = 0;
-
-        for (; instruction[i] != '\n' && instruction[i] != '\0'; i++, j++) rs2_str[j] = instruction[i];
-        rs2_str[j] = '\0';
-        rs2 = atoi(rs2_str);
-
-        return_funct(op_str, &funct3, &funct7);
-
-        result |= op;
-        result |= (rd << 7);
-        result |= (funct3 << 12);
-        result |= (rs1 << 15);
-        result |= (rs2 << 20);
-        result |= (funct7 << 25);
-    }
-    // #### TYPE I
-    else if (op == 0x13){    
-        j = 0;
-        while(instruction[i] != 'x') i++;
-        i++;
-        for (; instruction[i] != ','; i++, j++) rd_str[j] = instruction[i];
-        rd_str[j] = '\0';
-        rd = atoi(rd_str);
-        while(instruction[i] != 'x') i++;
-        i++;
-        j = 0;
-
-        for (; instruction[i] != ','; i++, j++) rs1_str[j] = instruction[i];
-        rs1_str[j] = '\0';
-        rs1 = atoi(rs1_str);
-        while(instruction[i] == ' ') i++;
-        i++;
-        j = 0;
-
-        for (; instruction[i] != '\n' && instruction[i] != '\0'; i++, j++) imm_i_str[j] = instruction[i];
-        imm_i_str[j] = '\0';
-        imm_i = atoi(imm_i_str);
-        //if(DEBUG) printf("imm = 0x%08X\n", imm_i);
-
-        return_funct3(op_str, &funct3);
-
-        result |= op;
-        result |= (rd << 7);
-        result |= (funct3 << 12);
-        result |= (rs1 << 15);
-        result |= ((imm_i & 0xFFF) << 20);
-    }
-    // #### TYPE B  
-    else if (op == 0x63) {
-        j = 0;
-        while(instruction[i] != 'x') i++;
-        i++;
-        for (; instruction[i] != ','; i++, j++) rs1_str[j] = instruction[i];  
-        rs1_str[j] = '\0';
-        rs1 = atoi(rs1_str);
-        j = 0;
-        
-        while(instruction[i] != 'x') i++;
-        i++;
-        for (; instruction[i] != ','; i++, j++) rs2_str[j] = instruction[i];  
-        rs2_str[j] = '\0';
-        rs2 = atoi(rs2_str);
-        j = 0;
-        
-        while (instruction[i] == ' ' || instruction[i] == ',') i++;
-        char label[MAX_CHARACTERS];
-        for (; instruction[i] != '\n' && instruction[i] != '\0'; i++, j++) label[j] = instruction[i];
-        label[j] = '\0'; 
-        int imm =  return_label_offset(label, current_pc);
-        return_funct3(op_str, &funct3);
-
-        result |= (((imm >> 12) & 0x01) << 31); // imm[12]
-        result |= (((imm >> 5)  & 0x3F) << 25); // imm[10:5]
-        result |= (((imm >> 1)  & 0x0F) << 8);  // imm[4:1]
-        result |= (((imm >> 11) & 0x01) << 7);  // imm[11]
-
-        result |= op;
-        result |= (funct3 << 12);
-        result |= (rs1 << 15);
-        result |= (rs2 << 20);
-    }
-    else{
-        printf("unsupported instruction\n");
-        return INVALID_INSTRUCTION;
-    }
-
-    return result;
+void close(Assembler* as){
+    fclose(as->instructions);
+    fclose(as->machine_code);
+    for (int i = 0; i < as->nlabels; i++)
+        free(as->labels[i].name);
+    free(as->labels);
 }
 
 int main(){
-    initialize();
+    Assembler as;
 
-    if (instructions == NULL){
-        printf("failed to open instructions.txt\n");
-        return 1;
-    } 
-
-    if (code == NULL){
-        printf("failed to open machine_code.txt\n");
-        return 2;
-    }
-
-    first_pass();
-    rewind(instructions); // reset the instruction pointer to the beginning of the file
-
-
-    uint32_t inst;
-    char str[MAX_CHARACTERS];
-    char inst_str[32];
-    int line = 0;
-
-    while(fgets(str, MAX_CHARACTERS, instructions) != NULL){
-        inst = assemble(str);
-        line++;
-        if (inst == LABEL_FOUND){
-            if(DEBUG) printf("label\n");
-            continue;
-        }
-        if (inst == INVALID_INSTRUCTION){
-            printf("invalid instruction on line %d\n", line);
-            exit(1);
-        }
-        sprintf(inst_str, "0x%08X\n", inst);
-        fputs(inst_str, code);
-        if (DEBUG) print_hex(inst);
-    }
-    if (DEBUG) print_labels();
-    
-    fclose(instructions);
-    fclose(code);
-    for (int i = 0; i < label_list_size; i++) free(label_list[i].name);
-    free(label_list);
+    initialize(&as, "instructions.txt", "machine_code.txt");
+    first_pass(&as);
+    second_pass(&as);
+    close(&as);
 
     return 0;
 }
